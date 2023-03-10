79d955d63ef8c00544371ce82fda5c2e61eb69c2
I-E-S-U <ian.samayoausher@gmail.com>
2023-03-10 08:45:46 +0100

    Removed Comments, Rescaled Background

--- SuperKaboom.js
+++ SuperKaboom.js
@@ -1,20 +1,15 @@
- // import kaboom lib
- import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs";
+import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs";
     
  function loadBackground(backgroundName){
     add([
         sprite(backgroundName),
-        scale(width()/250, height()/160)
+        scale(width()/220, height()/220)
     ]);
  }
- // initialize kaboom context
  kaboom();
 
  loadSprite("grass", "assets/Grass.png");
 
- 
- 
- // add a piece of text at position (120, 80)
  scene("game", ()=>{
    loadBackground("grass");
  });
