9e5c7442bb263398c7977bd930ffbb138c63d1f9
I-E-S-U <ian.samayoausher@gmail.com>
2023-03-10 14:07:24 +0100

    move and player

--- SuperKaboom.js
+++ SuperKaboom.js
@@ -6,20 +6,51 @@ import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs";
         scale(width()/220, height()/220)
     ]);
  }
+
+ function instantiatePlayer(){
+  add([
+    z(5),
+    sprite("player"),
+    pos(100, 200),
+    area(),
+    body(),
+    health(8),
+    doubleJump(),
+    move(),
+    // Plain strings are tags, a quicker way to let us define behaviors for a group
+    "player",
+    "friendly",
+    // Components are just plain objects, you can pass an object literal as a component.
+    {
+        dir: LEFT,
+        dead: false,
+        speed: 240,
+    },
+  ]);
+ }
  kaboom();
 
  document.body.style.overflow = 'hidden';
 
- loadSprite("grass", "assets/Grass.png");
+loadSprite("grass", "assets/Grass.png");
 
-loadSprite("obunga", "assets/OIP.png")
+loadSprite("obunga", "assets/OIP.png");
+
+loadSprite("player", "assets/Ball.png");
+
+let player = null;
 
 scene("game", ()=>{
    loadBackground("grass"),
 add([
     sprite("obunga")
     ])
+player = instantiatePlayer();
 });
 
 
 go("game");
+
+onKeyDown("w", ()=> {
+  player.move(0, 100)
+})
