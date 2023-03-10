d785b9dd6674a2f6af1ad31431f6b159b2d75da6
I-E-S-U <ian.samayoausher@gmail.com>
2023-03-10 13:05:59 +0100

    Scrollbargon,Obunga,Dialog&Backgroundscale

--- SuperKaboom.js
+++ SuperKaboom.js
@@ -12,18 +12,7 @@ import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs";
 
  loadSprite("grass", "assets/Grass.png");
 
-loadSprite("obunga", "assets/OIP.png", {
-    anims: {
-        run: {
-            from: 0,
-            to: 300,
-        },
-        jump: {
-            from: 3,
-            to: 300,
-        },
-    },
-})
+loadSprite("obunga", "assets/OIP.png")
 
 scene("game", ()=>{
    loadBackground("grass"),
